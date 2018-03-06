b95a77f8cefc8571775696ac165365b8b2133a5e
James <azrealjames@gmail.com>
2018-03-06 13:12:37 -0700

    changed js file

--- gtable.js
+++ gtable.js
@@ -4,29 +4,51 @@ var key = "https://docs.google.com/spreadsheets/d/193O02HNKF_u8iQnbpTDElohhz16jU
 // punctuation or numbers in your column name
 // "title" is the column name you want to appear in the published table
 var columns = [{
- "data": "Resource",
- "title": "Resource"
+  "data": "Resource",
+  "title": "Resource"
 }, {
- "data": "ResourceType",
- "title": "Resource Type"
+  "data": "ResourceType",
+  "title": "Resource Type"
 }, {
- "data": "Description",
- "title": "Description"
+  "data": "Description",
+  "title": "Description"
 }, {
- "data": "Link",
- "title": "Web Address"
+  "data": "Link",
+  "title": "Web Address"
 }];
+$(document).ready(function() {
 
-//Initialize the DataTable object and put settings in
-$("#resourceTable").DataTable({
- ...
- //uncomment these options to simplify your table
- //"paging": false,
- //"searching": false,
- //"info": false
+  function initializeTabletopObject() {
+    Tabletop.init({
+      key: key,
+      callback: function(data, tabletop) {
+        writeTable(data); //call up datatables function
+      },
+      simpleSheet: true,
+      debug: false
+    });
+  }
+  initializeTabletopObject();
+
+  function writeTable() {
+
+    $('#resourceTable').html('<table cellpadding="0" cellspacing="0" border="0" class="table table-condensed table-bordered	table-striped table-hover" id="mySelection"></table>');
+
+    //Initialize the DataTable object and put settings in
+    $("#resourceTable").DataTable({
+      // "autoWidth": false;
+      "data": data,
+      "columns": columns,
+      "order": [
+        [2, "desc"]
+      ], //order on second column
+      "pagingType": "simple" //no page numbers
+      //uncomment these options to simplify your table
+      //"paging": false,
+      //"searching": false,
+      //"info": false
+    });
+  }
 });
-
-$('#resourceTable').html('<table cellpadding="0" cellspacing="0" border="0" class="table table-condensed table-bordered	table-striped table-hover" id="mySelection"></table>');
-
 
 // end of writeTable
